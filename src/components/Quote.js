bedf5710653a6efc60ad04b5a710e2b9fb9bdef1
sonndy <123dykai@gmail.com>
2022-01-19 21:57:02 -0500

    new component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,21 +1,37 @@
 import React, {useState} from 'react';
-
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
+import SaveQuote from './SaveQuote';
 
 const Quote = ({quotes, index, nextQuote, previousQuote})=> {
-    // const [text, author] = quotes[index]
+    const [saveQuote, setSaveQuote] = useState([])
+
+    const newSaveQuote = () => {
+        saveQuote.push(quotes[index])
+        let uniqueQuote = [...new Set(saveQuote)];
+        setSaveQuote(uniqueQuote)
+        
+    }
     return(
         
-        <div className='main'>
-            <p>{quotes[index].text}</p>
-            <div className='buttons'>
-                <button onClick={()=> {
-                    nextQuote(index);
-                }}>Next</button>
-                <button onClick={()=> {
-                    previousQuote(index)
-                }}>Previous</button>
-            </div>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/">
+                    <div className='main'>
+                        <p>{quotes[index].text}</p>
+                        <div className='buttons'>
+                            <button onClick={()=> {
+                                nextQuote(index);
+                            }}>Next</button>
+                            <button onClick={()=> {
+                                previousQuote(index)
+                            }}>Previous</button>
+                            <button onClick={newSaveQuote}>Save Quote</button>
+                            {console.log(saveQuote)}
+                        </div>
+                    </div>
+                </Route>
+            </Routes>
+        </Router>
     )
 }
 
---
+++ src/components/SaveQuote.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+
+const SaveQuote = () => {
+    return (
+        <div>
+            <h1>Sonndy is the best</h1>
+        </div>
+    )
+}
+
+
+export default SaveQuote
